94f90ec4c7f867b63a172b0bcacc01b5aee5b257
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-10-30 18:48:45 +0100

    chore:specificate the time running on print

--- example_00_more_time.py
+++ example_00_more_time.py
@@ -14,34 +14,34 @@ async def greeting_per_minute():
     """to each minute tell a random greet"""
 
     greet_list = ['Hello', 'Good Morning', 'Good Evening', 'Good bye']
-    print(choice(greet_list))
+    print(choice(greet_list), " minute")
 
 @scheduler.task('hourly')
 async def greeting_per_hour():
     """to each hourly tell a random greet"""
 
     greet_list = ['Hello', 'Good Morning', 'Good Evening', 'Good bye']
-    print(choice(greet_list))
+    print(choice(greet_list), " hourly")
 
 @scheduler.task('daily')
 async def greeting_per_day():
     """to each daily tell a random greet"""
 
     greet_list = ['Hello', 'Good Morning', 'Good Evening', 'Good bye']
-    print(choice(greet_list))
+    print(choice(greet_list), " daily")
 
 @scheduler.task('weekly')
 async def greeting_per_week():
     """to each weekly tell a random greet"""
 
     greet_list = ['Hello', 'Good Morning', 'Good Evening', 'Good bye']
-    print(choice(greet_list))
+    print(choice(greet_list), " weekly")
 
 @scheduler.task('monthly')
 async def greeting_per_month():
     """to each monthly tell a random greet"""
 
     greet_list = ['Hello', 'Good Morning', 'Good Evening', 'Good bye']
-    print(choice(greet_list))
+    print(choice(greet_list), " monthly")
 
 scheduler.run()
